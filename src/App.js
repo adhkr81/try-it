64679720ea197b5a1168fe1dd63897b5004b8239
unknown <adhkr81@gmail.com>
2023-01-11 16:57:04 -0300

    try it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles.module.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <div className={styles.button}>///</div>
+                <div className={styles.menu}>"menu"</div>
+                <div className={styles.device}>"device"</div>
+                <div className={styles.steps}>"steps"</div>
+            </div>
+
+            <div className={styles.footnote}>
+                Disclaimer: Some functions are only available on the Galaxy S22 Ultra.
+            </div>
+        </div>
     </div>
   );
 }
